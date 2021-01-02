49c122011b2f313ea4c3d7c8d2a6d9d7a2bb0901
Jordi Casesnoves <jordicasesnoves@gmail.com>
2021-01-02 19:23:34 +0100

    added getCountryDataByDate func

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,27 @@
 import { useState, useEffect } from 'react';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 
-import { getCountries, getDataByDateRange } from '../../services/api';
+import { getCountryDataByDate } from '../../services/api';
 
 const HomePage = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    getDataByDateRange('2020-12-22', '2020-12-23')
+    // 2020-03-22
+    // yyyy-mm-dd
+
+    const today = new Date();
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1;
+    const yyyy = today.getFullYear();
+    const todayDate = `${yyyy}-${mm}-${dd}`;
+
+    getCountryDataByDate(todayDate, 'Spain')
       .then((res) => res.json())
       .catch((err) => console.log(err))
       .then((data) => {
+        console.log(data);
         setData(data);
         setLoading(false);
       });
